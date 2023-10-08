fcca5768fd5255d263cf8db561d8d9d10038410a
samchyshyn <samrich@ukr.net>
2023-10-08 12:35:56 +0300

    create homework

--- explisit_wate.py
+++ explisit_wate.py
@@ -5,25 +5,16 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
-# говоримо    шукати кожен елемент на протязі 5 секунд
-driver.implicitly_wait(2)
 driver.maximize_window()
 try:
 	print("Browser Start!")
-	driver.get("https://casenik.com.ua/user/login")	# открываем страницу
+	driver.get("https://poltava.ortosano.com.ua")		# открываем страницу
 
-	inpUt_email = driver.find_element(By.XPATH, "//input[@id = 'email']")
-	inpUt_email.send_keys("igor2@gmail.com")
-	password_ = driver.find_element(By.XPATH, "//input[@id = 'pasword']")
-	password_.send_keys("qwer432!")
-	login_button = driver.find_element(By.XPATH, '//button[@class = "btn button-gen"]').click()
-
-	massage = WebDriverWait(driver, 28).until_not(
-		# EC.element_to_be_clickable((By.XPATH, "//div[@class = 'alert alert-success']"))
-		EC.visibility_of_element_located((By.XPATH, "//div[@class = 'alert alert-success']"))
-	)	# ждем 30 сек пока не исчезнет строка по Х пасу
-
-	time.sleep(5)
+	massage=WebDriverWait(driver, 13).until(				# ждем 13 сек пока появиться строка по CSS -->
+	# EC.element_to_be_clickable((By.XPATH, "//div[@class = 'alert alert-success']"))
+	EC.visibility_of_element_located((By.CSS_SELECTOR, "div[class*=tp-linesplit]"))
+	)
+	# на 10 секундах и меньше тест не проходит
 finally:
 	driver.quit()
 	print("Browser quit!")
